9825637fb0ae4088dd1f8af47a9357303a20df7e
Alan Hernandez <yondaime_97@live.com>
2026-04-25 00:43:23 -0600

    feat: implement ExpensesController and integrate expense routes in BudgetRouter

--- backend/src/routes/BudgetRouter.ts
+++ backend/src/routes/BudgetRouter.ts
@@ -3,6 +3,7 @@ import { BudgetController } from '../controllers/BudgetController';
 import { body, param } from 'express-validator';
 import { handleInputErrors } from '../middleware/validation';
 import { validateBudgetExist, validateBudgetId, validateBudgetInput } from '../middleware/Budget';
+import { ExpensesController } from '../controllers/ExpenseController';
 
 const router = Router();
 
@@ -18,5 +19,12 @@ router.put('/:budgetId', validateBudgetInput, BudgetController.updateById);
 
 router.delete('/:budgetId', BudgetController.deleteById);
 
+
+// Routes for Expenses
+router.get('/:budgetId/expenses', ExpensesController.getAll)
+router.post('/:budgetId/expenses', ExpensesController.create)
+router.get('/:budgetId/expenses/:expenseId', ExpensesController.getById)
+router.put('/:budgetId/expenses/:expenseId', ExpensesController.updateById)
+router.delete('/:budgetId/expenses/:expenseId', ExpensesController.deleteById)
 
 export default router;
